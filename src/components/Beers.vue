9b76db1a458aea8bdad2bd201538c25d9ad11175
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-03-05 09:28:35 +0100

    render beers and add beer search

--- src/components/Beers.vue
+++ src/components/Beers.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="beers">
       <h1>Beers</h1>
+      <div class="beer" v-for="beer of beers" v-bind:key="beer.id">
+        <h2>{{ beer.name }}</h2>
+        <img v-bind:src="beer.image_url">
+        <p>{{ beer.description }}</p>
+      </div>
   </div>
 </template>
 
 <script>
+
+import BeerService from '../services/BeerService.js';
+
 export default {
   name: 'Beers',
+  data: () => {
+    return {
+      beers: []
+    }
+  },
+  mounted () {
+     this.beerService = new BeerService();
+     this.beerService.getBeers().then((beers) => {
+       this.beers = beers;
+     })
+  }
 }
 </script>
 
--- src/services/BeerService.js
+++ src/services/BeerService.js
@@ -1,17 +1,21 @@
-export class BeerService {
+export default class {
 
     beers;
+    url = 'https://api.punkapi.com/v2/beers';
 
     filterBeers () {
 
     }
 
     getBeers () {
-
+        return fetch(this.url).then(response => {
+            this.beers = response.json();
+            return this.beers;
+        })
     }
 
     searchBeers () {
-        
+
     }
 
 } 
